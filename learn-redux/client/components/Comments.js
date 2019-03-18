fffee98401a674d810ebc152426f17400669bcc5
Didac Bigorda <didacbigorda@gmail.com>
2019-03-18 20:21:44 +0100

    Add handle submit form function for comments component

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 export default class Comments extends Component {
+
   renderComment(comment, i) {
     return (
       <div className="comment" key={i}>
@@ -12,11 +13,20 @@ export default class Comments extends Component {
       </div>
     )
   }
+
+  handelSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const  author = this.refs.author.nodeValue;
+    const comment = this.refs.comment.nodeValue;
+    this.props.addComment(postId, author, comment);
+  }
+
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handelSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
           <input type="submit" hidden />
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,4 +1,5 @@
 const comments = (state = [], action) => {
+  
   return state;
 }
 
