13d3d017c710958ac824dec40a7b4ad79a3f62b1
Didac Bigorda <didacbigorda@gmail.com>
2019-03-18 20:15:22 +0100

    Add comment display

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -4,13 +4,15 @@ import Comments from './Comments';
 
 export default class Single extends Component {
   render() {
+    const { postId } = this.props.params;
     const i = this.props.posts.findIndex((post) =>
-      post.code === this.props.params.postId);
+      post.code === postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || []; // So we can loop over an empty array if no comments or push new comments in
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
